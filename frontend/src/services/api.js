e915a54706aa41d45365ea127d9c0624a08dfe7f
Anirban Sarkar <anirbansarkar.slg18@gmail.com>
2025-06-30 11:38:26 +0530

    fix: Update API baseURL to use environment variable for better configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ // env 
-  baseURL: 'http://localhost:8000/api/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE,
+  headers: { 'Content-Type': 'application/json' },
 })
 
 export default api
